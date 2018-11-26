2489f0e39cecd2d6d7b88d052238cd61d3b732fe
Michael Nutt <mnutt346@gmail.com>
2018-11-26 11:26:15 -0600

    Complete 6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends HoneyMakerBee {
+  constructor () {
+    super();
+    this.age = 10,
+    this.job = 'find pollen',
+    this.canFly = true,
+    this.treasureChest = [],
+    this.forage = function (treasure) {
+      this.treasureChest.push(treasure);
+    }
+  }
 };
